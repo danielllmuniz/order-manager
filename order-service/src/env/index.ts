dc11a18c2b81b6f9c62e61318dba9e65c5c658df
daniel muniz <munidaniel@gmail.com>
2025-11-28 00:11:39 -0300

    config: ts setup

---
+++ order-service/src/env/index.ts
@@ -0,0 +1,16 @@
+import 'dotenv/config';
+import { z } from 'zod';
+
+const envSchema = z.object({
+});
+
+
+const _env = envSchema.safeParse(process.env);
+
+if (!_env.success) {
+  console.error("❌ Invalid environment variables:");
+  console.error(z.treeifyError(_env.error));
+  throw new Error("Invalid environment variables");
+}
+
+export const env = _env.data;
