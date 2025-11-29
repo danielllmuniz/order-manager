486829d7d8a4af2e9eca0410874ab512783b2199
daniel muniz <munidaniel@gmail.com>
2025-11-29 01:13:54 -0300

    chore: implemented routes

--- order-service/src/infrastructure/api/express.ts
+++ order-service/src/infrastructure/api/express.ts
@@ -1,3 +1,6 @@
 import express, { Express } from 'express';
+import { orderRoute } from './routes/order.route';
 
 export const app: Express = express();
+app.use(express.json());
+app.use('/order', orderRoute);
---
+++ order-service/src/infrastructure/api/routes/order.route.ts
@@ -0,0 +1,19 @@
+import express, { Request, Response } from 'express';
+
+export const orderRoute = express.Router();
+
+orderRoute.post('/', async (req: Request, res: Response) => {
+  res.send('create');
+});
+
+orderRoute.get('/:id', async (req: Request, res: Response) => {
+  res.send('find by id');
+});
+
+orderRoute.patch('/:id/status', async (req: Request, res: Response) => {
+  res.send('update status');
+});
+
+orderRoute.get('/', async (req: Request, res: Response) => {
+  res.send('list all orders');
+});
